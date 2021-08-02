22f4206630df727569018c72d64d6d955a9fe077
Caballero75 <43910549+Caballero75@users.noreply.github.com>
2021-08-02 17:22:32 -0300

    Sless +Sfull widgets

---
+++ bricks/fulw/__brick__/snakeCase}}.dart
@@ -0,0 +1,15 @@
+class {{#pascalCase}}{{name}}{{/pascalCase}} extends StatefulWidget {
+	{{#pascalCase}}{{name}}{{/pascalCase}}({Key? key}) : super(key: key);
+
+	@override
+	_{{#pascalCase}}{{name}}{{/pascalCase}}State createState() => _{{#pascalCase}}{{name}}{{/pascalCase}}State();
+}
+
+class _{{#pascalCase}}{{name}}{{/pascalCase}}State extends State<{{#pascalCase}}{{name}}{{/pascalCase}}> {
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			 child: null,
+		);
+	}
+}
